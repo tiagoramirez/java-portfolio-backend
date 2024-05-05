3474d9f8e442a61cad7df30f692582ed5b3591a3
Tiago Ramirez <tiagoramirez2001@gmail.com>
2024-05-05 00:18:00 -0300

    initial commit

---
+++ src/main/java/com/tiagoramirez/portfolio/PortfolioV3Application.java
@@ -0,0 +1,13 @@
+package com.tiagoramirez.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagoramirez/portfolio/PortfolioV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagoramirez.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
